375ec7dcdd8b10c6ee5b346e0481ec8d54216fe5
Alan ps <alanps3699@gmail.com>
2026-02-24 12:42:11 +0530

    Added channels component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { UserTable } from './user-table/user-table';
 import { Rooms } from './rooms/rooms';
+import { Dashboard  } from './dashboard/dashboard';
+import {Channels} from './channels/channels';
 
 export const routes: Routes = [
+    { path: 'channels', component:Channels},
+    { path:'dashboard',component:Dashboard },
     { path:'rooms', component: Rooms },
     { path:'', component: UserTable }
 ];
